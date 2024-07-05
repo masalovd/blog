1cab5a2799a92fd2039c516ffc52b0548eadc5f3
Dmytro Masalov <dmitriymasalov5@gmail.com>
2024-07-05 11:12:25 +0300

    Add profile display and update

--- blog/config/urls.py
+++ blog/config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from users import views as users_views
+from . import settings
 
 
 urlpatterns = [
@@ -12,3 +14,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('feed.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
