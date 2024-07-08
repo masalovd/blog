019a63dda2ad959a75f669a0510b4366fb207198
Dmytro Masalov <dmitriymasalov5@gmail.com>
2024-07-08 11:19:15 +0300

    Change PostListView to order posts from new to old

--- blog/feed/views.py
+++ blog/feed/views.py
@@ -29,6 +29,9 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 5
     
+    def get_queryset(self):
+            return Post.objects.all().order_by('-created_at') 
+
 
 class UserPostListView(ListView):
     model = Post
